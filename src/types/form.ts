aeca69369f7a26ec1497919f450f8ab360629756
Gautam Kumar <gautam.kumar@submittable.com>
2026-08-24 16:55:46 +0530

    PSV-6564: SAI-CFA MUI dependencies assessment

---
+++ src/types/form.ts
@@ -0,0 +1,13 @@
+export interface IntakeFormData {
+  LOCATION_NAME?: string;
+  MARKET_DFA_NAME?: string;
+  OPERATOR?: string;
+  REGION?: string;
+  STORE_NUMBER?: string;
+  SCHOLARSHIP_NAME?: string;
+}
+
+export interface ScholarshipOption {
+  id: string;
+  name: string;
+}
